5dc0c5077f57b625dae7233eb53aebdda1e18382
KakashiSensei <vivekfitkariwala@gmail.com>
2017-06-24 04:39:17 +0530

    Adding decode in helmet

--- src/react/headers/GameHeader.js
+++ src/react/headers/GameHeader.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Helmet, title } from "react-helmet";
 import * as config from "../../config";
 import { PropTypes } from "prop-types";
+import decode from "urldecode";
 
 export default class GameHeader extends Component {
     static propTypes = {
@@ -22,7 +23,7 @@ export default class GameHeader extends Component {
             <Helmet>
                 <meta charSet="utf-8" />
                 <title>White Light</title>
-                <meta name="description" content={title} />
+                <meta name="description" content={decode(title)} />
                 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
                 <meta name="viewport" content="width=device-width, height=device-height,  initial-scale=1" />
                 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
--- src/react/headers/ResultHeader.js
+++ src/react/headers/ResultHeader.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Helmet, title } from "react-helmet";
 import * as config from "../../config";
 import { PropTypes } from "prop-types";
+import decode from "urldecode";
 
 export default class ResultHeader extends Component {
 
@@ -20,7 +21,7 @@ export default class ResultHeader extends Component {
             <Helmet>
                 <meta charSet="utf-8" />
                 <title>White Light</title>
-                <meta name="description" content={this.props.title} />
+                <meta name="description" content={decode(this.props.title)} />
                 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
                 <meta name="viewport" content="width=device-width, height=device-height,  initial-scale=1" />
                 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
